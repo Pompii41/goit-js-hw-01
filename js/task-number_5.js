f044d307a528305c848b5497c17f59a91961906e
Pompii41 <grevtsov.serezha@gmail.com>
2020-07-10 17:52:22 +0300

    dvs

--- js/task-number_5.js
+++ js/task-number_5.js
@@ -3,19 +3,19 @@ const country = prompt(`В какую страну доставить товар
 if (country === null) {
   console.log("Отменено пользователем!");
 } else {
-  let summary = country[0].toUpperCase() + country.slice(1);
+  let sammuryCountry = `${country[0].toUpperCase()}${country.slice(1)}`;
   switch (country.toLowerCase()) {
     case "китай":
-      console.log(`Доставка в ${summary} будет стоить 100 кредитов`);
+      console.log(`Доставка в ${sammuryCountry} будет стоить 100 кредитов`);
       break;
     case "чили":
-      console.log(`Доставка в ${summary} будет стоить 250 кредитов`);
+      console.log(`Доставка в ${sammuryCountry} будет стоить 250 кредитов`);
       break;
     case "австралия":
-      console.log(`Доставка в ${summary} будет стоить 170 кредитов`);
+      console.log(`Доставка в ${sammuryCountry} будет стоить 170 кредитов`);
       break;
     case "ямайка":
-      console.log(`Доставка в ${summary} будет стоить 120 кредитов`);
+      console.log(`Доставка в ${sammuryCountry} будет стоить 120 кредитов`);
       break;
     default:
       console.log(`В вашей стране доставка не доступна`);
